474f8294622958a6e21f4aa58bb3a2759b2315a7
Mike Slinn <mslinn@mslinn.com>
2025-05-23 20:20:23 -0400

    -

--- spec/yaml_parser_spec.rb
+++ spec/yaml_parser_spec.rb
@@ -31,35 +31,29 @@ RSpec.describe(JekyllSupport::YamlParser) do
   end
 
   it 'finds section headings' do
+    # includes leading zeros and leading spaces, which are invalid in YAML
     yaml_parser = described_class.new <<~END_DATA
-       0: Production Infrastructure
-      015000: Audio
-       20000: Video
-       30000: RME
-       40000: OBS Studio
-       50000: Pro Tools
-       55000: Ableton Live &amp; Push
-       60000: Other Music Software
-       70000: MIDI Hardware &amp; Software
-       80000: Davinci Resolve
-       100000: Computer Analysis
-       200000: Music Theory
-       300000: Business
-       400000: General
+        0: Production Infrastructure
+      0015000: Audio
+        20000: Video
+        30000: RME
+        40000: OBS Studio
+        50000: Pro Tools
+        55000: Ableton Live &amp; Push
+        60000: Other Music Software
+        70000: MIDI Hardware &amp; Software
+        80000: Davinci Resolve
+        100000: Computer Analysis
+        200000: Music Theory
+        300000: Business
+        400000: General
     END_DATA
 
     sections = yaml_parser.sections
     expect(sections.count).to equal(14)
-  end
 
-  # it 'The first section heading looks ok' do
-  #   first_heading = yaml_parser.sections.first
-  #   # expect(first_heading.).to equal(14)
-  # end
-
-  it 'parses yaml' do
-    expected = ''
-    actual = ''
-    expect(expected).to match_ignoring_whitespace(actual)
+    section1 = sections.first
+    expect(section1.order).to eq(0)
+    expect(section1.title).to eq('Production Infrastructure')
   end
 end
