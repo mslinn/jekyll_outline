bd8e029630731b7ff16bd943e370af2508fbfcee
Mike Slinn <mslinn@mslinn.com>
2025-05-30 11:48:04 -0400

    -

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -3,11 +3,13 @@ require 'jekyll_plugin_logger'
 require 'jekyll_plugin_support'
 
 RSpec.configure do |config|
-  # See https://www.rubydoc.info/github/rspec/rspec-core/RSpec%2FCore%2FConfiguration:pending_failure_output
-  config.pending_failure_output = :skip
-  config.filter_run_when_matching focus: true
-  config.order = 'random'
-
   # See https://relishapp.com/rspec/rspec-core/docs/command-line/only-failures
   config.example_status_persistence_file_path = 'spec/status_persistence.txt'
+
+  config.filter_run_when_matching focus: true
+  config.formatter = :documentation # :progress, :html, :json, CustomFormatterClass
+  config.order = 'random'
+
+  # See https://www.rubydoc.info/github/rspec/rspec-core/RSpec%2FCore%2FConfiguration:pending_failure_output
+  config.pending_failure_output = :skip
 end
